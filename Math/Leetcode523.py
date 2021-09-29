6acb6521e112e5bb8bda53d41e17b39f8741f8a1
yw-scu <49891048+yw-scu@users.noreply.github.com>
2021-09-29 17:43:40 +0200

    [Similar questions]: Leetcode523, 560,1477

--- Math/Leetcode523.py
+++ Math/Leetcode523.py
@@ -1,15 +1,15 @@
 class Solution:
     def checkSubarraySum(self, nums, k: int) -> bool:
-        dp=[0]
+        dp={0:[-1]}
         sum = 0
         for i in range(len(nums)):
             sum += nums[i]
-            if sum!=0 and sum%k==0:
-                return True
-            for j in range(i):
-                if (sum-dp[j])%k==0:
-                    return True
-            dp.append(sum)
+            sum%=k
+            if dp.get(sum) is not None:
+                for j in dp[sum]:
+                    if i-j>1:
+                        return True
+            dp[sum]=[i] if dp.get(sum) is None else dp[sum]+[i]
         return False
 
 if __name__ == '__main__':
@@ -18,6 +18,10 @@ if __name__ == '__main__':
     # k = 6
     # nums = [23, 2, 6, 4, 7]
     # k = 6
-    nums=[0]
-    k=1
+    # nums=[0]
+    # k=1
+    # nums=[1]
+    # k=1
+    nums=[5, 0, 0, 0]
+    k=3
     print(sol.checkSubarraySum(nums,k))
--- SlidingWindows/Leetcode1477.py
+++ SlidingWindows/Leetcode1477.py
@@ -1,83 +1,28 @@
 class Solution:
     def minSumOfLengths(self, arr, target: int) -> int:
-        res = []
+        best=[float('inf') for i in arr]
+        sum=0
+        res=float('inf')
+        bestSoFar=float('inf')
+        start=0
+
         for i in range(len(arr)):
-            right=i
-            count=0
-            while right<len(arr):
-                count+=arr[right]
-                right+=1
-                if count==target:
-                    if len(res) == 0:
-                        res.append(right - i)
-                    elif len(res) == 1:
-                        if right - i > res[0]:
-                            res.append(right - i)
-                        else:
-                            res = [right - i] + res
-                    else:
-                        if right - i < res[0]:
-                            res = [right - i] + res
-                            res.pop()
-                        elif right - i < res[-1]:
-                            res.pop()
-                            res.append(right - i)
-        # left = right = 0
-        # count = 0
-        # while right < len(arr):
-        #     c = arr[right]
-        #     right += 1
-        #     count += c
-        #     if count == target:
-        #         if len(res) == 0:
-        #             res.append(right - left)
-        #         elif len(res) == 1:
-        #             if right - left > res[0]:
-        #                 res.append(right - left)
-        #             else:
-        #                 res = [right - left] + res
-        #         else:
-        #             if right - left < res[0]:
-        #                 res = [right - left] + res
-        #                 res.pop()
-        #             elif right - left < res[-1]:
-        #                 res.pop()
-        #                 res.append(right - left)
-        #         left = right
-        #         count = 0
-        #     elif count > target:
-        #         while count > target and left < len(arr):
-        #             count -= arr[left]
-        #             left += 1
-        #         if count == target:
-        #             if len(res) == 0:
-        #                 res.append(right - left)
-        #             elif len(res) == 1:
-        #                 if right - left > res[0]:
-        #                     res.append(right - left)
-        #                 else:
-        #                     res = [right - left] + res
-        #             else:
-        #                 if right - left < res[0]:
-        #                     res = [right - left] + res
-        #                     res.pop()
-        #                 elif right - left < res[-1]:
-        #                     res.pop()
-        #                     res.append(right - left)
-        #             left = right
-        #             count = 0
+            sum+=arr[i]
+            while sum>target:
+                sum-=arr[start]
+                start+=1
+            if sum==target:
+                if start>0:
+                    res=min(res,best[start-1]+i-start+1)
+                bestSoFar=min(bestSoFar,i-start+1)
+            best[i]=bestSoFar
 
-        return sum(res) if len(res) == 2 else -1
-
+        return res if res<float('inf') else -1
 
 if __name__ == '__main__':
-    sol = Solution()
+    sol=Solution()
     # arr = [3, 2, 2, 4, 3]
     # target = 3
-    # arr = [4, 3, 2, 6, 2, 3, 4]
-    # target = 6
-    # arr = [1, 2, 2, 3, 2, 6, 7, 2, 1, 4, 8]
-    # target = 5
-    arr=[2, 1, 3, 3, 2, 3, 1]
-    target=6
-    print(sol.minSumOfLengths(arr, target))
+    arr = [5, 5, 4, 4, 5]
+    target = 3
+    print(sol.minSumOfLengths(arr,target))
